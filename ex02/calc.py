8f55bbfb4f3fdbdfc9ff03af756fa3090a32813e
C0B21123 <c0b2112359@edu.teu.ac.jp>
2022-10-04 14:28:58 +0900

    練習2

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,13 +5,30 @@ import tkinter as tk#モジュールインポート
 import tkinter.messagebox as tkm
 root = tk.Tk()#tkモジュールの中のTKのインスタンスを生成
 
-def button_click(event):
-    btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+# def button_click(event):
+#     btn = event.widget
+#     txt = btn["text"]
+#     tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
 
-root.title("tk")
+root.title("tk")#練習1
 root.geometry("300x500")
+
+
+r,c = 0,0
+for num in range(9,-1,-1):
+    btn = tk.Button(root,text=f"{num}",font=("",30),width=4,height=2)
+    btn.grid(row=r,column=c)
+    c += 1
+    if c == 3:
+        c = 0
+        r += 1
+        
+        
+
+
+
+
+root.mainloop()#rootはインスタンス
 
 # root.geometry("500x200")
 
@@ -28,14 +45,14 @@ root.geometry("300x500")
 
 # tkm.showwarning("警告","はいやったなお前")
 
-button = tk.Button(root,text="押すな",font=("",40))
-button.bind("<1>",button_click)
-button.pack()
+# button = tk.Button(root,text="押すな",font=("",40))
+# button.bind("<1>",button_click)
+# button.pack()
 
 
 
 
 
-root.mainloop()#rootはインスタンス
+
 
 
