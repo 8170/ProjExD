e247e7d70fedd8077cfbf9a84cff95050e77d78f
C0B21123 <c0b2112359@edu.teu.ac.jp>
2022-10-25 15:36:10 +0900

    練習04-7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,6 +3,21 @@ import pygame as pg
 import sys
 import random
 
+def check_bound(obj_rct,scr_rct):
+    """
+    こうかとんrctまたは爆弾rctを想定している+スクリーンのrct
+    領域内ないなら+1、領域外なら-1
+    """
+    yoko,tate = 1,1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return (yoko,tate)
+
+
+
+
 def main():
     #練習1
     pg.display.set_caption("逃げろ！こうかとん")#タイトルバーに表記
@@ -32,7 +47,7 @@ def main():
     bomb_rct.centery = random.randint(0,scrn_rct.height)
 
     #練習6
-    vx,vy = 1,1
+    vx,vy = 4,4
 
 
     
@@ -50,16 +65,40 @@ def main():
             if event.type == pg.QUIT: return
 
         key_stats = pg.key.get_pressed()
-        if key_stats[pg.K_UP]: tori_rct.centery -= 3
-        if key_stats[pg.K_DOWN]: tori_rct.centery += 3
-        if key_stats[pg.K_LEFT]: tori_rct.centerx -= 3
-        if key_stats[pg.K_RIGHT]: tori_rct.centerx += 3
+        if key_stats[pg.K_UP]: 
+            tori_rct.centery -= 3
+        if key_stats[pg.K_DOWN]: 
+            tori_rct.centery += 3
+        if key_stats[pg.K_LEFT]: 
+            tori_rct.centerx -= 3
+        if key_stats[pg.K_RIGHT]: 
+            tori_rct.centerx += 3
         
-       
+        yoko,tate = check_bound(tori_rct,scrn_rct)
+        if yoko == -1:
+            if key_stats[pg.K_LEFT]:
+                tori_rct.centerx += 3
+            if key_stats[pg.K_RIGHT]:
+                tori_rct.centerx -= 3
+        if tate == -1:
+            if key_stats[pg.K_UP]:
+                tori_rct.centery += 3
+            if key_stats[pg.K_DOWN]:
+                tori_rct.centery -= 3
 
+    
+            
         scrn_sfc.blit(tori_sfc,tori_rct)#こうかとん貼り付け
 
+
+
+        yoko,tate = check_bound(bomb_rct,scrn_rct)#移動判定(爆弾)
+        vx *= yoko
+        vy *= tate
         bomb_rct.move_ip(vx,vy)
+
+
+
         scrn_sfc.blit(bomb_sfc,bomb_rct)#爆弾貼り付け
 
         
